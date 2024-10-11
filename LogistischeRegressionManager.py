113a84014707c8f457caf7eb76b4f428addaac25
RaphaDevx <raphael.markus@icloud.com>
2024-10-11 17:14:23 +0200

    Update LogistischeRegressionManager.py

--- LogistischeRegressionManager.py
+++ LogistischeRegressionManager.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import streamlit as st
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
+from scikit-learn.model_selection import train_test_split
+from scikit-learn.linear_model import LogisticRegression
 import pickle
 from Herz import Herz
 
